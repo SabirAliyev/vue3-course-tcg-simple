202b6d170af86ded5f46967d95b7b35beaabca3a
Sabir Aliyev <sabir.aliyev.itdep@gmail.com>
2025-02-14 09:47:59 +0400

    20. Understanding methods

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
             courseGoal: 'Finish the course and learn Vue!',
             vueLink: 'https://vuejs.org'
         };
+    },
+    methods: {
+        courseGoalResult() {
+            const randomNumber = Math.random();
+            if (randomNumber > 0.5) {
+                return "Learn Vue more!"
+            } else {
+                return "You are Vue Master!"
+            }
+        }
     }
 });
 
