6a850a3016877d800f5d7985c93ba596320df1fd
Sabir Aliyev <sabir.aliyev.itdep@gmail.com>
2025-02-14 13:31:43 +0400

    25. Understanding event binding with methods

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoalA: 'Finish the course and learn Vue!',
-            courseGoalB: '<h2>You are Vue Master already!</h2>',
-            vueLink: 'https://vuejs.org'
+            counter: 0,
         };
     },
     methods: {
-        courseGoalResult() {
-            const randomNumber = Math.random();
-            if (randomNumber > 0.5) {
-                return this.courseGoalA;
-            } else {
-                return this.courseGoalB;
-            }
+        add() {
+            this.counter++;
+        },
+        reduce() {
+            this.counter--;
         }
-    }
+    },
 });
 
-app.mount('#user-goal');
+app.mount('#events');
